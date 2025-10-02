00e5b48f2d471cf50ca64671e0b4773795c4ca3a
emirbeser <emirbeser2@gmail.com>
2025-10-02 19:45:01 +0300

    plus+plus

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.CategoryResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public async Task <List<GetCategoryQueryResult>> Handle()
+        {
+            var values = await _context.Categories.ToListAsync();
+            return values.Select(x=> new GetCategoryQueryResult
+            {
+                CategoryId = x.CategoryId,
+                CategoryName = x.CategoryName
+            }).ToList();
+        }
+    }
+}
---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+
+    }
+}
